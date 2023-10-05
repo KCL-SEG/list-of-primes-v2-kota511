312458bcd985ffc42310d93bcbc206d0467468d7
Kota Amemiya <kotaamemiya@gmail.com>
2023-10-05 12:27:10 +0100

    Modified ValueError to pass primes_test.py.

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(number):
+    increaser = int(2)
+    while increaser <= number/2:
+        if number % increaser == 0:
+            return False
+        else:
+            increaser += 1
+    return True
+
 def primes(number_of_primes):
+    if not isinstance(number_of_primes, int) or number_of_primes < 1:
+        raise ValueError("That wasn't an integer or not positive")
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
+
     return list
+
+inputs = int(input("Enter number of primes to get here:"))
+print(primes(inputs))
